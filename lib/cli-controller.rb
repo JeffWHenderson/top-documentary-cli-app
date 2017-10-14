6048f51be80e32ceaba81da50146535efba60d98
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-10-14 16:44:07 +0000

    set up menu stub,started to build scraper.

--- lib/cli-controller.rb
+++ lib/cli-controller.rb
@@ -1,3 +1,14 @@
 class Controller
+  def menu
+    #Scraper.new.scrape
+    puts "hello user what category would you like to browse\n\n\n"
+    input = gets.strip
+    puts "you are browsing #{input}"
+    puts "options in chosen category"
+  end
 
+  # 1. scrape from https://topdocumentaryfilms.com/
+  # 2. present a #menu
+  # 3. gets user input
+  # 4. shows #detail
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,8 @@
-class Scraper
+require "nokogiri"
+require "open-uri"
 
+class Scraper
+  def scrape_home_page(url = "https://topdocumentaryfilms.com/")
+    doc = Nokogiri::HTML(open(url))
+  end
 end
