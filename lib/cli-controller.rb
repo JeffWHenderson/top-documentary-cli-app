0658287309c4af7b6af93df5605110e707d63b77
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-10-14 19:54:16 +0000

    finished scrape home page functionality and collaboration with cli.

--- lib/cli-controller.rb
+++ lib/cli-controller.rb
@@ -1,10 +1,14 @@
 class Controller
   def menu
     #Scraper.new.scrape
-    puts "hello user what category would you like to browse\n\n\n"
+    puts "hello user what category would you like to browse?\n\n\n"
+    Scraper.new.scrape_home_page
+    puts "\n\nselect the number of the category you would like to browse" #Scraper.categories[0][0] #put this in a list categories method2
     input = gets.strip
-    puts "you are browsing #{input}"
-    puts "options in chosen category"
+    puts "you are browsing #{Scraper.categories[input.to_i - 1][1]}" # here you take user input and  open the category[input - 1][1]
+    puts "type 'list' to go back to main, enter number for more information on a documentary"
+    input = gets.strip
+    puts "you chose #{input}"
   end
 
   # 1. scrape from https://topdocumentaryfilms.com/
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,26 @@ require "nokogiri"
 require "open-uri"
 
 class Scraper
+attr_accessor
+    @@categories = []
+
+
   def scrape_home_page(url = "https://topdocumentaryfilms.com/")
     doc = Nokogiri::HTML(open(url))
+
+    category = doc.search("ul.cat-list a")
+    category.each_with_index do |x, i|
+      @@categories << ["#{x.text}", "#{x.attr('href')}"]
+      puts "#{i + 1}. #{x.text}"
+    end
+  end
+
+  # def scrape_category_url(category_url = "https://topdocumentaryfilms.com/")
+  #   doc = Nokogiri::HTML(open(category_url))
+  #   puts doc.css("article.module")
+  # end
+
+  def self.categories
+    @@categories
   end
 end
