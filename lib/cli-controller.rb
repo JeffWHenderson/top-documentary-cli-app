e3be8a265e9b2eef9e000265fe088fd80d6006e7
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-10-14 22:35:10 +0000

    set a call function to do some of the logic that was in menu. debugging an error that is preventing program from exiting.

--- lib/cli-controller.rb
+++ lib/cli-controller.rb
@@ -1,24 +1,44 @@
 class Controller
-  def test_menu
-    Scraper.new.scrape_category_url
+  def call
+    list_categories
+    menu
+    goodbye
   end
 
   def menu
     #Scraper.new.scrape
-    puts "hello user what category would you like to browse?\n\n\n"
+    input = nil
+    while input != 'exit'
+      puts "\n\nselect the number of the category you would like to browse or type 'exit'"
+      input = gets.strip # i have to make it to integer but that gets rid of the exit functionality
+
+      # if input.to_i > 0 && input.to_i < 26
+      #   input = input.to_i
+      #   puts "category : #{Scraper.categories[input - 1][0]}\n\n"
+      #   Scraper.new.scrape_category_url(Scraper.categories[input - 1][1])
+      # elsif input != 'exit'
+      #   puts "not a valid input. please enter the number by the category you want"
+      #   menu
+      # else
+      #   goodbye
+      # end
+      #
+      # puts "\n\n 'exit' to leave.  or you can enter 'list' for categories"
+      # input = gets.strip
+      # if input == 'list'
+      #   puts "#{input}"#call
+      # else
+      #   "invalid input. enter 'exit' to leave. or 'list' for categories"
+      # end
+    end
+  end
+
+  def list_categories
     Scraper.new.scrape_home_page
-    puts "\n\nselect the number of the category you would like to browse" #Scraper.categories[0][0] #put this in a list categories method2
+  end
 
-    input = gets.strip.to_i
-    puts "category : #{Scraper.categories[input - 1][0]}\n\n"
-    Scraper.new.scrape_category_url(Scraper.categories[input - 1][1])
-
-    puts "\n\n enter movie number if you want a link.  'exit' to leave.  or you can enter 'list' for categories"
-    input = gets.strip.to_i
-    #show movie objects url
-    #or go to list
-    #or exit
-    #this loop will end my program
+  def goodbye
+    puts "\n\n\ncome back soon"
   end
 
 end
